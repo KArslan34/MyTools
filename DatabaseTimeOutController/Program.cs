6dc5e211f8b3c0af90ad12a3ecb9202fba959d96
Kerem Arslan <54895983+KArslan34@users.noreply.github.com>
2026-09-01 22:35:50 +0300

    Add files via upload

---
+++ DatabaseTimeOutController/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+class Program
+{
+    public static void Main()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["SqlConn"].ConnectionString;
+
+        string query = "EXEC test";
+
+        using (SqlConnection conn = new(connectionString))
+        using (SqlCommand cmd = new(query, conn))
+        {
+            cmd.CommandTimeout = 0;
+
+            conn.Open();
+
+            Stopwatch sw = Stopwatch.StartNew();
+            cmd.ExecuteNonQuery();
+            sw.Stop();
+
+            Console.WriteLine($"Sorgu çalışma süresi: {sw.Elapsed.TotalSeconds} sn");
+            Console.WriteLine($"(ms): {sw.ElapsedMilliseconds}");
+        }
+
+        Console.ReadLine();
+    }
+}
